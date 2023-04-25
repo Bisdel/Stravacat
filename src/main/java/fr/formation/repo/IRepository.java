d88ed040adf549d317cee5025ef96dc2647b692c
Bisdel <bricotte.leo@gmail.com>
2023-04-25 10:48:46 +0200

    leo

--- src/main/java/fr/formation/repo/IRepository.java
+++ src/main/java/fr/formation/repo/IRepository.java
@@ -2,15 +2,13 @@ package fr.formation.repo;
 
 import java.util.List;
 
-import fr.formation.model.Animal;
-
 public interface IRepository<T> {
 
-    public List<Animal> findAll();
+    public List<T> findAll();
 
     public void createEntry(String pseudo);
     
-    public void updateEntry(Animal animal);
+    public void updateEntry(T t);
     
     public void deleteEntry();
 
