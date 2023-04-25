aa23855e6504f672531b614e4642e385ff281877
Bisdel <bricotte.leo@gmail.com>
2023-04-25 15:47:42 +0200

    undo changes IRepository

--- src/main/java/fr/formation/repo/IRepository.java
+++ src/main/java/fr/formation/repo/IRepository.java
@@ -2,15 +2,13 @@ package fr.formation.repo;
 
 import java.util.List;
 
-import fr.formation.model.Abonnes;
-
 public interface IRepository<T> {
 
     public List<T> findAll();
 
     public void createEntry();
     
-    public T updateEntry(T entity);
+    public void updateEntry(T entity);
     
     public void deleteEntry();
 
