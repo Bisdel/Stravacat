d4e2de7db72c7ab5c1ca34d2c3d5c9d181ce4fe9
yatchenko17 <ekaterina.yatchenko@outlook.fr>
2023-04-25 14:11:17 +0200

    commit maj

--- src/main/java/fr/formation/repo/jpa/AbstractRepositoryJpa.java
+++ src/main/java/fr/formation/repo/jpa/AbstractRepositoryJpa.java
@@ -1,23 +1,8 @@
 package fr.formation.repo.jpa;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public abstract class AbstractRepositoryJpa {
-    
-    protected Connection connection;
-
-    public AbstractRepositoryJpa() {
-        
-        try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/stravacat_db", "postgres", "0000");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    
-
+    protected static EntityManagerFactory emf = Persistence.createEntityManagerFactory("Stravacat");
 }
