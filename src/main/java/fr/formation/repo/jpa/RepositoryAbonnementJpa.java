9bd9d74c07295d6e701dacdb9a93c342b99b41db
Massi1996 <benchabamassipssa@gmail.com>
2023-04-26 10:11:10 +0200

    partiea abonnement

---
+++ src/main/java/fr/formation/repo/jpa/RepositoryAbonnementJpa.java
@@ -0,0 +1,40 @@
+package fr.formation.repo.jpa;
+
+import java.util.List;
+
+import fr.formation.model.Abonnement;
+import fr.formation.repo.IAbonnementRepository;
+
+public class RepositoryAbonnementJpa extends AbstractRepositoryJpa implements IAbonnementRepository {
+
+	@Override
+	public List<Abonnement> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createEntry() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateEntry(Abonnement entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteEntry() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Abonnement> findByPseudo(String pseudo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
