08a05d81e59dd65f813d7ad7892797a71b8abe90
Bisdel <bricotte.leo@gmail.com>
2023-05-02 10:37:22 +0200

    clean repo etcs.

--- src/main/java/fr/formation/repo/jpa/AbstractRepositoryJpa.java
+++
@@ -1,9 +0,0 @@
-package fr.formation.repo.jpa;
-
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-
-
-public abstract class AbstractRepositoryJpa {
-    protected static EntityManagerFactory emf = Persistence.createEntityManagerFactory("Stravacat-Unit");
-}
