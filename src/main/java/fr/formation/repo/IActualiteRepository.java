639c2d5d3da7a7f1ddda9c427ea5a8e49f737ee3
Bisdel <bricotte.leo@gmail.com>
2023-05-09 14:35:17 +0200

    hotfix

--- src/main/java/fr/formation/repo/IActualiteRepository.java
+++ src/main/java/fr/formation/repo/IActualiteRepository.java
@@ -1,10 +1,6 @@
 package fr.formation.repo;
 
-<<<<<<< HEAD
-import java.util.Optional;
-=======
 import java.util.List;
->>>>>>> ed4118af8ae274b32f0b5e55c2d5330433792cd5
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
