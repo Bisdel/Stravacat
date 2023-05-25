1183cc4343c89dfaed80c5987afe50e8f42a8b2b
Massi1996 <benchabamassipssa@gmail.com>
2023-05-25 10:40:47 +0200

    24

--- src/main/java/fr/formation/api/AbonnementApiController.java
+++ src/main/java/fr/formation/api/AbonnementApiController.java
@@ -1,5 +1,28 @@
 package fr.formation.api;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import fr.formation.api.response.AbonnementResponse;
+import fr.formation.repo.IAbonnementRepository;
+
+@Controller
+@RequestMapping("/api/Aboonement")
 public class AbonnementApiController {
+	
+	@Autowired
+	private IAbonnementRepository repoAbonnement;
+	
+	// Liste des Abonnement
+	@GetMapping
+	public List<AbonnementResponse> findAll(){
+		return this.repoAbonnement.findAll().stream().map(AbonnementResponse :: convert).toList();
+	}
+	
+	
 
 }
