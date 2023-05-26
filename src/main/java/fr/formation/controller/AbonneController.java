72ce9ec82a410c5e2cd3be0bb098527305ed9cdb
Massi1996 <benchabamassipssa@gmail.com>
2023-05-26 11:58:45 +0200

    34

---
+++ src/main/java/fr/formation/controller/AbonneController.java
@@ -0,0 +1,59 @@
+package fr.formation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import fr.formation.exception.AbonnesNotFoundException;
+import fr.formation.model.Abonnes;
+import fr.formation.repo.IAbonnesRepository;
+import jakarta.validation.Valid;
+
+@Controller
+public class AbonneController {
+
+	@Autowired
+	private IAbonnesRepository repoAbonnes;
+
+	@GetMapping("abonne")
+	public String findAll(Model model) {
+		model.addAttribute("abonnes", this.repoAbonnes.findAll());
+
+		return "abonne/liste";
+	}
+
+	@PostMapping({ "/abonne/ajouter", "/abonne/modifier/{id}" })
+	public String add(@Valid Abonnes abonnes, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			model.addAttribute("erreurs", result);
+			return "abonne/ajouter";
+		}
+
+		this.repoAbonnes.save(abonnes);
+
+		return "redirect:/abonne";
+	}
+
+	@GetMapping("/abonne/modifier/{id}")
+	public String edit(@PathVariable int id, Model model) {
+		Abonnes abonne = this.repoAbonnes.findById(id).orElseThrow(AbonnesNotFoundException::new);
+		model.addAttribute("abonne", abonne);
+		return "abonne/ajouter";
+	}
+
+	@GetMapping("/abonne/supprimer/{id}")
+	public String deletById(@PathVariable int id) {
+		try {
+			this.repoAbonnes.deleteById(id);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return "redirect:/abonne?erreursup";
+		}
+		return "redirect:/abonne";
+	}
+
+}
---
+++ src/main/java/fr/formation/controller/AbonnementController.java
@@ -0,0 +1,59 @@
+package fr.formation.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import fr.formation.exception.AbonnementNotFoundException;
+import fr.formation.model.Abonnement;
+import fr.formation.repo.IAbonnementRepository;
+import jakarta.validation.Valid;
+
+@Controller
+public class AbonnementController {
+
+	@Autowired
+	private IAbonnementRepository repoAbonnement;
+	
+	@GetMapping("Abonnement")
+	public String findAll(Model model ) {
+		model.addAttribute("aboonement" , this.repoAbonnement.findAll());
+		
+		return "abonnement/liste";
+	}
+	
+	@PostMapping({"/abonnement/ajouter" ,"/abonnement/modifier/{id}"})
+	public String add(@Valid Abonnement abonnement, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			model.addAttribute("abonnement" , result);
+			return "abonnement/ajouter";
+		}
+		
+		this.repoAbonnement.save(abonnement);
+		
+		return "redirect:/abonnement";
+	}
+	
+	@GetMapping("/abonnement/modfier/{id}")
+	public String edit(@PathVariable int id , Model model) {
+		Abonnement abonnement = this.repoAbonnement.findById(id).orElseThrow(AbonnementNotFoundException :: new);
+		model.addAttribute("abonnement", abonnement);
+		return "abonnement/ajouter";
+	}
+	
+	@GetMapping("/abonnement/supprimer/{id}")
+	public String deletByid(@PathVariable int id) {
+		try {
+			this.repoAbonnement.deleteById(id);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return "redirect:/abonnement?erreursup";
+		}
+		
+		return "redirect:/abonnement";
+	}
+}
