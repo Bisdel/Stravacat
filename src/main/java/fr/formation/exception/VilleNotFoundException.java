b7afc0f9cb36e0cdbb0d13f9e53156b64ca1b101
Bisdel <bricotte.leo@gmail.com>
2023-05-31 14:46:34 +0200

    implementation Spring Boot

--- src/main/java/fr/formation/exception/VilleNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package fr.formation.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
-public class VilleNotFoundException extends RuntimeException{
-
-}
