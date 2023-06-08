8fd84a9dfe094cbd10dcb296eb3ef6f1239afde7
Massi1996 <benchabamassipssa@gmail.com>
2023-06-08 11:07:05 +0200

    ma partie

--- angular/src/app/models/abonne.ts
+++ angular/src/app/models/abonne.ts
@@ -1,2 +1,58 @@
+import { Animal } from "./animal";
+import { Ville } from "./ville";
+
 export class Abonne {
+    public get animal(): Animal[] {
+        return this._animal;
+    }
+    public set animal(value: Animal[]) {
+        this._animal = value;
+    }
+    public get ville(): Ville[] {
+        return this._ville;
+    }
+    public set ville(value: Ville[]) {
+        this._ville = value;
+    }
+    public get espece(): string {
+        return this._espece;
+    }
+    public set espece(value: string) {
+        this._espece = value;
+    }
+    public get age(): number {
+        return this._age;
+    }
+    public set age(value: number) {
+        this._age = value;
+    }
+    public get pseudo(): string {
+        return this._pseudo;
+    }
+    public set pseudo(value: string) {
+        this._pseudo = value;
+    }
+    public get id(): number {
+        return this._id;
+    }
+    public set id(value: number) {
+        this._id = value;
+    }
+
+    constructor(
+        private _id: number,
+        private _pseudo: string,
+        private _age: number,
+        private _espece: string,
+        private _ville: Ville[],
+        private _animal: Animal[]
+    ){ };
+    
+
+
+
+
+
+
 }
+
