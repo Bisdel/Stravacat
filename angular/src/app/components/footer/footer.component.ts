ba98a44f9be56944403d54f5f67f33bffa1cd468
Bisdel <bricotte.leo@gmail.com>
2023-06-12 10:05:21 +0200

    fix erreurs missipssa + footer

--- angular/src/app/components/footer/footer.component.ts
+++ angular/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  authOk:boolean = this.srvAuth.isLogged();
 
+  constructor(private srvAuth: AuthenticationService) {
+  }
+
+  logout(){
+    this.srvAuth.logout();
+  }
 }
